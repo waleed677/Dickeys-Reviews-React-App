42a3a56f13ea754fd6c0b369d0377119acb89136
waleed677 <waleedmalik677@gmail.com>
2023-10-10 15:27:39 +0500

    design header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header-bg'>
+      <nav className="flex max-w-7xl items-center justify-center p-4" >
+      <div >
+        <img src="assets/images/logo.png" alt="" />
+      </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
