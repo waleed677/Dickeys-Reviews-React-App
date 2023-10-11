aae72820b4697c6e2fa0dd9d84a5008df4ee71e2
waleed677 <waleedmalik677@gmail.com>
2023-10-11 16:22:19 +0500

    Add Questionaires Data

---
+++ src/data/questionaire.js
@@ -0,0 +1,10 @@
+export const Questionaires = [
+    {
+        question : 'Sorry to hear that !!! Where did you fall short?',
+        options: ['Accuracy', 'Cleanliness', 'Food Quality', 'Taste of Food', 'Friendliness','Speed of Services','Staff Behaviour','Other']
+    },
+    {
+        question : 'Where did we miss on cleanliness?',
+        options: ['Chairs', 'Drink Bar', 'Floors', 'Tables', 'Friendliness','Something else']
+    }
+]
