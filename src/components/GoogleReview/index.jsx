8d93e9e555d14f17f5d8dba54e86740ad845ddae
waleed677 <waleedmalik677@gmail.com>
2023-10-15 15:52:10 +0500

    update google review link

--- src/components/GoogleReview/index.jsx
+++ src/components/GoogleReview/index.jsx
@@ -7,7 +7,7 @@ const GoogleReview = () => {
     const [showSuggestion, setShowSuggestion] = useState(false)
 
     const handleGoogleReview = () => {
-        window.open("https://www.google.com/maps/place/Dickey's+Barbecue+Pit/@43.6924776,-116.4856379,17z/data=!4m8!3m7!1s0x54afad186b65df0d:0xc9d92511304aacff!8m2!3d43.6924776!4d-116.4856379!9m1!1b1!16s%2Fg%2F11sd_y6w6c?entry=ttu", '_self')
+        window.open("https://www.google.com/search?q=Dickey%27s+Barbecue+Pit%2C+10580+W+State+St%2C+Star%2C+ID+83669%2C+United+States&oq=Dickey%27s+Barbecue+Pit%2C+10580+W+State+St%2C+Star%2C+ID+83669%2C+United+States&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDEwMjJqMGo3qAIAsAIA&client=ms-android-xiaomi-rvo2&sourceid=chrome-mobile&ie=UTF-8#lrd=0x54afad186b65df0d:0xc9d92511304aacff,3,,,,&pi=dickey's%20barbecue%20pit%20star%20reviews&sbfbu=0&wptab=si:ALGXSlb9ylv6FQuI8zCROKWgJkMHV_7qC6_qVQjaGVH5mNU2SfoBIch9Av48LmNl-WdUFIQ9DSHT9Tshg4NqcfSZZsf5Pm6vTEHRmeOsfx8e8GwOBvrn7664m7sEpMT0vz1BX7O21ZYvywJjqhQxvZYQKQ08LM4tnqQeHOHsYFJhnE1cffLKt1M%3D", '_self')
     }
 
     const handleShowSuggestion = () => {
